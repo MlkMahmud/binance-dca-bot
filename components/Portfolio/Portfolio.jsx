91dfac966181a30b8609fc88794c3684d00c1d5b
Malik Mahmud <almalikmahmud@gmail.com>
2021-08-28 23:53:51 +0100

    lazy load Portfolio components

---
+++ components/Portfolio/Portfolio.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Flex,
+  Grid,
+  Icon,
+  IconButton,
+  Text,
+} from '@chakra-ui/react';
+import { HiEye, HiEyeOff, HiRefresh } from 'react-icons/hi';
+import Select from 'react-select';
+import PropTypes from 'prop-types';
+import { generateSelectOption } from '../../utils';
+
+export default function Portfolio({
+  assets,
+  handleChange,
+  selectedSymbol,
+}) {
+  const [isBalanceHidden, setIsBalanceHidden] = useState(false);
+  const { free, locked } = assets.find(({ symbol }) => symbol === selectedSymbol);
+  return (
+    <Box
+      bgColor="#FFF"
+      border="1px solid #DADCE0"
+      borderRadius="5px"
+      maxW="650px"
+    >
+      <Flex
+        align="center"
+        borderBottom="1px solid #f0f0f0"
+        justify="space-between"
+        p={2.5}
+      >
+        <Text
+          as="h3"
+          fontSize="2xl"
+          fontWeight="semibold"
+        >
+          Portfolio
+        </Text>
+        <Box>
+          <IconButton
+            aria-label={`${isBalanceHidden ? 'show' : 'hide'} balance`}
+            h={['35px', '40px']}
+            icon={<Icon as={isBalanceHidden ? HiEyeOff : HiEye} boxSize={['25px', '30px']} />}
+            onClick={() => setIsBalanceHidden(!isBalanceHidden)}
+            minW={['35px', '40px']}
+            variant="unstyled"
+          />
+          <IconButton
+            aria-label="refresh portfolio"
+            h={['35px', '40px']}
+            icon={<Icon as={HiRefresh} boxSize={['25px', '30px']} />}
+            minW={['35px', '40px']}
+            variant="unstyled"
+          />
+        </Box>
+      </Flex>
+      <Box
+        p="15px 10px"
+      >
+        <Grid
+          alignItems="center"
+          columnGap="2%"
+          templateColumns="auto 1fr"
+          mb="20px"
+        >
+          <Text fontWeight="bold">Asset:</Text>
+          <Select
+            onChange={(({ value }) => handleChange(value))}
+            value={generateSelectOption(selectedSymbol)}
+            options={assets.map(({ symbol }) => generateSelectOption(symbol))}
+          />
+        </Grid>
+        <Flex align="center" justify="space-between" mb="5px">
+          <Text fontWeight="semibold">Free:</Text>
+          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${free} ${selectedSymbol}`}</Text>
+        </Flex>
+        <Flex align="center" justify="space-between" mb="5px">
+          <Text fontWeight="semibold">Locked:</Text>
+          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${locked} ${selectedSymbol}`}</Text>
+        </Flex>
+      </Box>
+      <Flex
+        align="center"
+        borderTop="1px solid #f0f0f0"
+        justify="space-between"
+        p={2.5}
+      >
+        <Text fontWeight="semibold">Total:</Text>
+        <Text fontWeight="medium">{isBalanceHidden ? '***' : `${Number(free + locked).toFixed(2)} ${selectedSymbol}`}</Text>
+      </Flex>
+    </Box>
+  );
+}
+
+Portfolio.propTypes = {
+  assets: PropTypes.arrayOf(PropTypes.shape({
+    symbol: PropTypes.string,
+    free: PropTypes.number,
+    locked: PropTypes.number,
+  })).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  selectedSymbol: PropTypes.string.isRequired,
+};
--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -1,95 +1,36 @@
-import {
-  Box,
-  Flex,
-  Grid,
-  Icon,
-  IconButton,
-  Text,
-} from '@chakra-ui/react';
-import Select from 'react-select';
-import React, { useState } from 'react';
-import { HiEye, HiEyeOff, HiRefresh } from 'react-icons/hi';
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
 
-export default function Portfolio() {
-  const [balances] = useState([]);
-  const [isBalanceHidden, setIsBalanceHidden] = useState(false);
-  const [selectedAsset, setSelectedAsset] = useState('USDT');
-  const currentAsset = balances.find(({ asset }) => asset === selectedAsset);
+const Loading = dynamic(() => import('./Loading'), { ssr: false });
+const Portfolio = dynamic(() => import('./Portfolio'), { ssr: false });
+
+export default function Component() {
+  const [assets, updateAssets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedSymbol, setSelectedSymbol] = useState('USDT');
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+      updateAssets([
+        { free: 2000, locked: 450, symbol: 'USDT' },
+        { free: 5, locked: 2, symbol: 'BNB' },
+        { free: 1240, locked: 0, symbol: 'DOGE' },
+        { free: 2, locked: 0.5, symbol: 'BTC' },
+      ]);
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
-    <Box
-      bgColor="#FFF"
-      maxW="650px"
-      shadow="rgb(0 0 0 / 16%) 0px 1px 2px -2px, rgb(0 0 0 / 12%) 0px 3px 6px 0px, rgb(0 0 0 / 9%) 0px 5px 12px 4px"
-    >
-      <Flex
-        align="center"
-        borderBottom="1px solid #f0f0f0"
-        justify="space-between"
-        p={2.5}
-      >
-        <Text
-          as="h3"
-          fontSize="2xl"
-          fontWeight="semibold"
-        >
-          Portfolio
-        </Text>
-        <Box>
-          <IconButton
-            aria-label={`${isBalanceHidden ? 'show' : 'hide'} balance`}
-            h={['35px', '40px']}
-            icon={<Icon as={isBalanceHidden ? HiEyeOff : HiEye} boxSize={['25px', '30px']} />}
-            onClick={() => setIsBalanceHidden(!isBalanceHidden)}
-            minW={['35px', '40px']}
-            variant="unstyled"
-          />
-          <IconButton
-            aria-label="refresh portfolio"
-            h={['35px', '40px']}
-            icon={<Icon as={HiRefresh} boxSize={['25px', '30px']} />}
-            minW={['35px', '40px']}
-            variant="unstyled"
-          />
-        </Box>
-      </Flex>
-      <Box
-        p="15px 10px"
-      >
-        <Grid
-          alignItems="center"
-          columnGap="2%"
-          templateColumns="auto 1fr"
-          mb="20px"
-        >
-          <Text fontWeight="bold">Asset:</Text>
-          <Select
-            onChange={(({ value }) => setSelectedAsset(value))}
-            value={selectedAsset}
-            options={balances.map(({ asset }) => ({
-              label: asset,
-              value: asset,
-            }))}
-          />
-        </Grid>
-        <Flex align="center" justify="space-between" mb="5px">
-          <Text fontWeight="semibold">Free:</Text>
-          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${currentAsset?.free} ${selectedAsset}`}</Text>
-        </Flex>
-        <Flex align="center" justify="space-between" mb="5px">
-          <Text fontWeight="semibold">Locked:</Text>
-          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${currentAsset?.locked} ${selectedAsset}`}</Text>
-        </Flex>
-      </Box>
-      <Flex
-        align="center"
-        borderTop="1px solid #f0f0f0"
-        justify="space-between"
-        p={2.5}
-      >
-        <Text fontWeight="semibold">Total:</Text>
-        <Text fontWeight="medium">{isBalanceHidden ? '***' : `${currentAsset?.total} ${selectedAsset}`}</Text>
-      </Flex>
-    </Box>
+    <Portfolio
+      assets={assets}
+      handleChange={setSelectedSymbol}
+      selectedSymbol={selectedSymbol}
+    />
   );
 }
