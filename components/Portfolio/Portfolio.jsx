e3fa3bb481c659d8d926467b0deef6ea64b4a3d1
Malik Mahmud <almalikmahmud@gmail.com>
2021-08-31 19:50:46 +0100

    chore(Portfolio): replace react-select with chakra select

--- components/Portfolio/Portfolio.jsx
+++ components/Portfolio/Portfolio.jsx
@@ -5,20 +5,17 @@ import {
   Grid,
   Icon,
   IconButton,
+  Select,
   Text,
 } from '@chakra-ui/react';
 import { HiEye, HiEyeOff, HiRefresh } from 'react-icons/hi';
-import Select from 'react-select';
 import PropTypes from 'prop-types';
-import { generateSelectOption } from '../../utils';
 
-export default function Portfolio({
-  assets,
-  handleChange,
-  selectedSymbol,
-}) {
+export default function Portfolio({ assets, handleChange, selectedSymbol }) {
   const [isBalanceHidden, setIsBalanceHidden] = useState(false);
-  const { free, locked } = assets.find(({ symbol }) => symbol === selectedSymbol);
+  const { free, locked } = assets.find(
+    ({ symbol }) => symbol === selectedSymbol,
+  );
   return (
     <Box
       bgColor="#FFF"
@@ -32,18 +29,19 @@ export default function Portfolio({
         justify="space-between"
         p={2.5}
       >
-        <Text
-          as="h3"
-          fontSize="2xl"
-          fontWeight="semibold"
-        >
+        <Text as="h3" fontSize="2xl" fontWeight="semibold">
           Portfolio
         </Text>
         <Box>
           <IconButton
             aria-label={`${isBalanceHidden ? 'show' : 'hide'} balance`}
             h={['35px', '40px']}
-            icon={<Icon as={isBalanceHidden ? HiEyeOff : HiEye} boxSize={['25px', '30px']} />}
+            icon={(
+              <Icon
+                as={isBalanceHidden ? HiEyeOff : HiEye}
+                boxSize={['25px', '30px']}
+              />
+            )}
             onClick={() => setIsBalanceHidden(!isBalanceHidden)}
             minW={['35px', '40px']}
             variant="unstyled"
@@ -57,9 +55,7 @@ export default function Portfolio({
           />
         </Box>
       </Flex>
-      <Box
-        p="15px 10px"
-      >
+      <Box p="15px 10px">
         <Grid
           alignItems="center"
           columnGap="2%"
@@ -68,18 +64,27 @@ export default function Portfolio({
         >
           <Text fontWeight="bold">Asset:</Text>
           <Select
-            onChange={(({ value }) => handleChange(value))}
-            value={generateSelectOption(selectedSymbol)}
-            options={assets.map(({ symbol }) => generateSelectOption(symbol))}
-          />
+            onChange={(e) => handleChange(e.target.value)}
+            value={selectedSymbol}
+          >
+            {assets.map(({ symbol }) => (
+              <option key={symbol} value={symbol}>
+                {symbol}
+              </option>
+            ))}
+          </Select>
         </Grid>
         <Flex align="center" justify="space-between" mb="5px">
           <Text fontWeight="semibold">Free:</Text>
-          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${free} ${selectedSymbol}`}</Text>
+          <Text fontWeight="medium">
+            {isBalanceHidden ? '***' : `${free} ${selectedSymbol}`}
+          </Text>
         </Flex>
         <Flex align="center" justify="space-between" mb="5px">
           <Text fontWeight="semibold">Locked:</Text>
-          <Text fontWeight="medium">{isBalanceHidden ? '***' : `${locked} ${selectedSymbol}`}</Text>
+          <Text fontWeight="medium">
+            {isBalanceHidden ? '***' : `${locked} ${selectedSymbol}`}
+          </Text>
         </Flex>
       </Box>
       <Flex
@@ -89,18 +94,24 @@ export default function Portfolio({
         p={2.5}
       >
         <Text fontWeight="semibold">Total:</Text>
-        <Text fontWeight="medium">{isBalanceHidden ? '***' : `${Number(free + locked).toFixed(2)} ${selectedSymbol}`}</Text>
+        <Text fontWeight="medium">
+          {isBalanceHidden
+            ? '***'
+            : `${Number(free + locked).toFixed(2)} ${selectedSymbol}`}
+        </Text>
       </Flex>
     </Box>
   );
 }
 
 Portfolio.propTypes = {
-  assets: PropTypes.arrayOf(PropTypes.shape({
-    symbol: PropTypes.string,
-    free: PropTypes.number,
-    locked: PropTypes.number,
-  })).isRequired,
+  assets: PropTypes.arrayOf(
+    PropTypes.shape({
+      symbol: PropTypes.string,
+      free: PropTypes.number,
+      locked: PropTypes.number,
+    }),
+  ).isRequired,
   handleChange: PropTypes.func.isRequired,
   selectedSymbol: PropTypes.string.isRequired,
 };
