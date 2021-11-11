692560e859baeef6d4e17531941ea7d404f10181
Malik Mahmud <almalikmahmud@gmail.com>
2021-11-11 20:54:09 +0100

    chore: move agenda jobs definition to separate file

---
+++ server/lib/agenda/jobs/buy-crypto.js
@@ -0,0 +1,26 @@
+import binance from '../../binance';
+import rootLogger from '../../logger';
+import sentry from '../../sentry';
+
+const logger = rootLogger.child({ module: 'agenda' });
+
+module.exports = (agenda) => {
+  agenda.define('buy-crypto', async (job) => {
+    try {
+      const { data } = job.attrs;
+      logger.info({ data }, `> Running ${data.jobName}`);
+      await binance.order({
+        symbol: data.symbol,
+        side: 'BUY',
+        type: 'MARKET',
+        newOrderRespType: 'FULL',
+        quoteOrderQty: data.amount,
+      });
+    // save order to db with with a ref field(jobId)
+    // send notification message
+    } catch (err) {
+      logger.error({ err });
+      sentry.captureException(err);
+    }
+  });
+};
