97720ddad5c3655242134e104899e27137a46452
Malik Mahmud <almalikmahmud@gmail.com>
2021-11-16 22:39:32 +0100

    fix css margin issue on loading overlay

--- components/Jobs/JobList.jsx
+++ components/Jobs/JobList.jsx
@@ -25,44 +25,46 @@ export default function JobList({ defaultTimezone, jobs }) {
   };
 
   return (
-    <Stack spacing={3}>
-      <Text fontSize="xl" fontWeight="bold">{`Jobs(${jobs.length})`}</Text>
-      <Grid
-        gap="20px"
-        templateColumns="repeat(auto-fill, minmax(min(100%, 400px), 450px))"
-      >
-        {jobs.map((job) => (
-          <Job
-            job={job}
-            key={job.id}
-            onEdit={() => openJobForm(job)}
-          />
-        ))}
-      </Grid>
-      <IconButton
-        aria-label="add new job"
-        bgColor="black"
-        borderRadius="50%"
-        bottom="40px"
-        color="white"
-        colorScheme="black"
-        height="60px"
-        icon={<Icon as={FaPlus} boxSize="24px" />}
-        onClick={() => openJobForm()}
-        pos="fixed"
-        right="20px"
-        width="60px"
-        variant="unstyled"
-      />
-      {isOpen && (
-        <JobForm
-          defaultTimezone={defaultTimezone}
-          onFormClose={handleClose}
-          isOpen={isOpen}
-          job={selectedJob}
+    <>
+      <Stack spacing={3}>
+        <Text fontSize="xl" fontWeight="bold">{`Jobs(${jobs.length})`}</Text>
+        <Grid
+          gap="20px"
+          templateColumns="repeat(auto-fill, minmax(min(100%, 400px), 450px))"
+        >
+          {jobs.map((job) => (
+            <Job
+              job={job}
+              key={job.id}
+              onEdit={() => openJobForm(job)}
+            />
+          ))}
+        </Grid>
+        <IconButton
+          aria-label="add new job"
+          bgColor="black"
+          borderRadius="50%"
+          bottom="40px"
+          color="white"
+          colorScheme="black"
+          height="60px"
+          icon={<Icon as={FaPlus} boxSize="24px" />}
+          onClick={() => openJobForm()}
+          pos="fixed"
+          right="20px"
+          width="60px"
+          variant="unstyled"
         />
+      </Stack>
+      {isOpen && (
+      <JobForm
+        defaultTimezone={defaultTimezone}
+        onFormClose={handleClose}
+        isOpen={isOpen}
+        job={selectedJob}
+      />
       )}
-    </Stack>
+    </>
   );
 }
 
