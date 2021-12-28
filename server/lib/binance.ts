2532aa93d1a47d8fdc5e6b46683d77f6e6a46337
Malik Mahmud <almalikmahmud@gmail.com>
2021-12-28 20:49:35 +0100

    chore: fix typos

--- server/lib/binance.ts
+++ server/lib/binance.ts
@@ -1,12 +1,11 @@
 import Binance from 'binance-api-node';
 
-const config = {
+const config: any = {
   apiKey: process.env.BINANCE_API_KEY,
   apiSecret: process.env.BINANCE_API_SECRET,
 };
 
 if (process.env.NODE_ENV !== 'production') {
-  // @ts-ignore
   config.httpBase = 'https://testnet.binance.vision';
 }
 
