2912d44d72e01a2a6b360cde04075a1e9be21662
Malik Mahmud <almalikmahmud@gmail.com>
2022-01-04 09:03:50 +0100

    CI: delete old source maps before uploading new

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,8 +4,9 @@ const { name } = require('../package.json');
 
 if (process.env.SENTRY_ENABLED === 'true' && process.env.SENTRY_DSN) {
   const cliPath = resolve('./node_modules/.bin/sentry-cli');
-  const release = `${name}@$latest`;
+  const release = `${name}@current`;
   sh.exec(`${cliPath} releases new ${release}`);
+  sh.exec(`${cliPath} releases files ${release} delete --all`);
   sh.exec(
     `${cliPath} releases files ${release} upload-sourcemaps ./.next ./dist`
   );
--- server/lib/sentry.ts
+++ server/lib/sentry.ts
@@ -6,7 +6,7 @@ const { name } = packageJson;
 sentry.init({
   dsn: process.env.SENTRY_DSN,
   enabled: process.env.SENTRY_ENABLED === 'true',
-  release: `${name}@latest`,
+  release: `${name}@current`,
 });
 
 export default sentry;
