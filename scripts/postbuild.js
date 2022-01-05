c5f71ffd478d20da40052cc6bab9e0f226ec54e6
Malik Mahmud <almalikmahmud@gmail.com>
2022-01-05 20:52:56 +0100

    chore: use env var to version sentry releases

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,10 +1,13 @@
 const { resolve } = require('path');
 const sh = require('shelljs');
-const { name } = require('../package.json');
+
+const version = process.env.PROJECT_VERSION
+  ? process.env.PROJECT_VERSION.trim()
+  : 'current';
 
 if (process.env.SENTRY_ENABLED === 'true' && process.env.SENTRY_DSN) {
   const cliPath = resolve('./node_modules/.bin/sentry-cli');
-  const release = `${name}@current`;
+  const release = `binance-dca-bot@${version}`;
   sh.exec(`${cliPath} releases new ${release}`);
   sh.exec(`${cliPath} releases files ${release} delete --all`);
   sh.exec(
